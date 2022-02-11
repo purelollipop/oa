090eb78a53ea7d52366b916515e1343229d1065e
hansicheng <hansisicheng@msgsz.com>
2022-02-11 14:36:08 +0800

    updata

--- src/api/get/get_api.ts
+++ src/api/get/get_api.ts
@@ -1,13 +1,19 @@
 import {sql} from '../../sql/sql_line'
 // let sql = require('../sql/sql_line')
-let sqlStr = {
-    login: 'SELECT * FROM ONE'
+
+interface sqlType{
+    login:string
+}
+let sqlStr:sqlType = {
+    login: 'SELECT * FROM user'
 }
 const getObj = {
-    login:async function(sqlStr:string,fun:()=>any){
+    login:async function(data:any):Promise<any>{
+        console.log('get_api',data)
         return new Promise((res:any,reject)=>{
-            sql.query('SELECT * FROM ONE',(error:any, results:Record<string, any>[], fields:any)=>{
+            sql.query(`SELECT * FROM user`,(error:any, results:Record<string, any>[], fields:any)=>{
                 if(error) return reject(error)
+                console.log(results)
                 res(results)
             })
         })
--- src/route/route.ts
+++ src/route/route.ts
@@ -1,6 +1,6 @@
 let {getObj} = require('../api/get/get_api')
 import fs from 'fs'
-
+let querystring = require('querystring');
 async function getFile(urlStr:string){
     return new Promise((resolve,reject)=>{
         fs.readFile(urlStr,'utf-8',(err,data)=>{
@@ -13,22 +13,32 @@ async function getFile(urlStr:string){
 }
 async function route(val:any):Promise<any>{
     let {url,method} = val
+    console.log(val)
     let arrUrl = url.split('/')
     url = arrUrl[arrUrl.length-1]
     if (url){
         switch (method){
             case 'GET': {
                 return getObj[url]().then((res:any)=>{
-                    console.log(res)
+                    return res
                 }).catch((err:any)=>{
                     return err
                 })
             }
-            case 'post':
-            // let sqlStr = 'SELECT * FROM thre'
-            // return await findFun(sqlStr).then(res=>{
-            //     return(res)
-            // })
+            case 'POST':
+                let data:any = ''
+                val.on('data',(chunk:any)=>{
+                    data += chunk
+                })
+                val.on('end',()=>{
+                    querystring.parse(data)
+                    // console.log('data``',data)
+                    return getObj[url](data).then((res:any)=>{
+                        return res
+                    }).catch((err:any)=>{
+                        return err
+                    })
+                })
             default:
                 return getFile('./dist/index.html').then(res=>{
                     return res
